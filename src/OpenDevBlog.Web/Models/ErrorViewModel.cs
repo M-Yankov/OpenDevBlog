b368dfae5da5d5be19ab087bddbe15c8a99ac6a2
M-Yankov <mihail.yankov.1337@gmail.com>
2019-03-11 23:00:19 +0200

    #13 Project scaffolding

---
+++ src/OpenDevBlog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OpenDevBlog.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
