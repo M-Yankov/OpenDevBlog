f396f3baad232b36269cf58f8174f0f607421e70
M-Yankov <mihail.yankov.1337@gmail.com>
2019-03-30 16:02:10 +0200

    #2 Integrate monaco-editor

---
+++ src/OpenDevBlog.Web/Controllers/ArticlesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenDevBlog.Web.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(object model)
+        {
+            return this.Redirect("Success");
+        }
+
+        [HttpGet]
+        public IActionResult Success()
+        {
+            return this.View();
+        }
+    }
+}
